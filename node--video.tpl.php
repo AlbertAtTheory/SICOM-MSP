9981cbaa7a6c5aaecd8c11a423b444590c4748ea
Albert Martin <albert@ilovetheory.com>
2013-01-28 11:55:09 -0500

    Made Garland a parent theme, removed redundant code.

--- node--video.tpl.php
+++ node--video.tpl.php
@@ -1,6 +1,5 @@
 <?php
 ?>
-
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print $user_picture; ?>
@@ -8,16 +7,17 @@
   <?php print render($title_prefix); ?>
   <?php if (!$page): ?>
     <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-
   <?php endif; ?>
   <?php print render($title_suffix); ?>
 
   <?php if ($display_submitted): ?>
     <span class="submitted"><?php print $submitted ?></span>
   <?php endif; ?>
+  
   <div class="view-header">
-	<h2>featured videos</h2>
-    </div>
+    <h2>Featured Videos</h2>
+  </div>
+
   <div class="content clearfix"<?php print $content_attributes; ?>>
     <?php
       // We hide the comments and links now so that we can render them later.
@@ -36,4 +36,3 @@
   </div>
 
 </div>
-
