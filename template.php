bfa30a43bac8139d4a1dd8017f4f940943da7e9b
Albert Martin <albert@ilovetheory.com>
2013-01-30 11:09:59 -0500

    Added states dropdown to dealer page.

--- template.php
+++ template.php
@@ -64,3 +64,76 @@ function msp_file_link($variables) {
 
   return '<span class="file">' . $icon . ' ' . l($link_text, $url, $options) . '</span>';
 }
+
+/**
+ * Override the Where to Buy exposed filters to show a dropdown menu.
+ */
+function msp_form_alter(&$form, &$form_state, $form_id) {
+  if($form['#id'] == 'views-exposed-form-where-to-buy-page') {  
+    $options = array(
+      '' => '--',
+      'Alabama' => 'Alabama',
+      'Alaska' => 'Alaska',
+      'American Samoa' => 'American Samoa',
+      'Arizona' => 'Arizona',
+      'Arkansas' => 'Arkansas',
+      'California' => 'California',
+      'Colorado' => 'Colorado',
+      'Connecticut' => 'Connecticut',
+      'Delaware' => 'Delaware',
+      'District of Columbia' => 'District of Columbia',
+      'Florida' => 'Florida',
+      'Georgia' => 'Georgia',
+      'Guam' => 'Guam',
+      'Hawaii' => 'Hawaii',
+      'Idaho' => 'Idaho',
+      'Illinois' => 'Illinois',
+      'Indiana' => 'Indiana',
+      'Iowa' => 'Iowa',
+      'Kansas' => 'Kansas',
+      'Kentucky' => 'Kentucky',
+      'Louisiana' => 'Louisiana',
+      'Maine' => 'Maine',
+      'Maryland' => 'Maryland',
+      'Massachusetts' => 'Massachusetts',
+      'Michigan' => 'Michigan',
+      'Minnesota' => 'Minnesota',
+      'Mississippi' => 'Mississippi',
+      'Missouri' => 'Missouri',
+      'Montana' => 'Montana',
+      'Nebraska' => 'Nebraska',
+      'Nevada' => 'Nevada',
+      'New Hampshire' => 'New Hampshire',
+      'New Jersey' => 'New Jersey',
+      'New Mexico' => 'New Mexico',
+      'New York' => 'New York',
+      'North Carolina' => 'North Carolina',
+      'North Dakota' => 'North Dakota',
+      'Northern Marianas Islands' => 'Northern Marianas Islands',
+      'Ohio' => 'Ohio',
+      'Oklahoma' => 'Oklahoma',
+      'Oregon' => 'Oregon',
+      'Pennsylvania' => 'Pennsylvania',
+      'Puerto Rico' => 'Puerto Rico',
+      'Rhode Island' => 'Rhode Island',
+      'South Carolina' => 'South Carolina',
+      'South Dakota' => 'South Dakota',
+      'Tennessee' => 'Tennessee',
+      'Texas' => 'Texas',
+      'Utah' => 'Utah',
+      'Vermont' => 'Vermont',
+      'Virginia' => 'Virginia',
+      'Virgin Islands' => 'Virgin Islands',
+      'Washington' => 'Washington',
+      'West Virginia' => 'West Virginia',
+      'Wisconsin' => 'Wisconsin',
+      'Wyoming' => 'Wyoming',
+    );
+  
+    $form['field_state_value']['#type'] = 'select';
+    $form['field_state_value']['#options'] = $options;
+    $form['field_state_value']['#default_value'] = array();
+    $form['field_state_value']['#multiple'] = FALSE;
+    $form['field_state_value']['#size'] = 1;
+  }
+}
